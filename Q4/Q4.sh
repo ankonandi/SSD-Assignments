d04b217c57eadf48546773381a8136bf542869fa
ankonandi <ankonandi5002@gmail.com>
2024-09-01 20:43:43 +0530

    added comments to the code

--- Q4/Q4.sh
+++ Q4/Q4.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#creates directory
 mkdir temp_activity
 
+#creates 50 files
 eval "touch temp_activity/temp{1..50}.sh"
 
+#renames first 25 file types from txt to sh
 for file in temp_activity/temp{1..25}.sh; do
     mv "$file" "${file%.sh}.txt"
 done
